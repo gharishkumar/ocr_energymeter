dbbd4bf4d0f21d88bb8f8c8ea6ac748ded68ad7f
Harish Kumar <g.harishkumar1998@gmail.com>
2022-01-05 08:27:55 +0530

    Add files via upload

---
+++ modify_xml.py
@@ -0,0 +1,17 @@
+import xml.etree.ElementTree as ET
+import sys
+
+mytree = ET.parse('log.xml')
+myroot = mytree.getroot()
+reading = float(sys.argv[1])
+for old_value in myroot.iter('OLD'):
+	for new_value in myroot.iter('NEW'):
+		old_value.text = new_value.text
+		for consumption in myroot.iter('CONSUMPTION'):
+			consumption.text = str(round(reading - float(new_value.text), 1))
+		new_value.text = str(reading)
+	
+
+
+
+mytree.write('log.xml')
